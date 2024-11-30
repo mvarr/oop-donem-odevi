47e3d0fbf59e6b4f477efd87c21b41f099b3d3c8
Melih Var <varmelih711@gmail.com>
2024-11-30 12:49:34 +0300

    update main.c

--- main.cpp
+++ main.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
-#include"function.cpp"
+#include "function.cpp"
 using namespace std;
 
 int main() {
-    
-    
+    Student s; // Varsayılan kapasite 200
+    s.readFromCSV();
+
+    string input;
+    cout << "Bir deger girin (0, 1 veya baska bir sey): ";
+    cin >> input;
+
+    s.print(input);
+    return 0;
 }
